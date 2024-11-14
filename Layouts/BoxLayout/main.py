79c01b6f66bcf0d17aa8ae67c10562506c2eb289
Ankit Dware <sabindware@gmail.com>
2024-11-14 14:41:09 +0545

    box layout

---
+++ Layouts/BoxLayout/main.py
@@ -0,0 +1,11 @@
+from kivy.app import App
+from kivy.uix.widget import Widget
+from kivy.uix.boxlayout import BoxLayout
+
+class BoxLayoutWidget(BoxLayout):
+    pass
+
+class LabApp(App):
+    pass
+
+LabApp().run()
