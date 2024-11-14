769c3301ac0ac1bf4837caeb0d6e1d5290df7415
Ankit Dware <sabindware@gmail.com>
2024-11-14 15:03:03 +0545

    grid layout

---
+++ Layouts/GridLayout/main.py
@@ -0,0 +1,12 @@
+
+from kivy.app import App
+from kivy.uix.widget import Widget
+from kivy.uix.gridlayout import GridLayout
+
+class GridLayoutWidget(GridLayout):
+    pass
+
+class LabApp(App):
+    pass
+
+LabApp().run()
