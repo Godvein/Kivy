f2daaf06cc3331e64d23a7acfe8cb74e0100ef7c
Ankit Dware <sabindware@gmail.com>
2024-11-14 14:54:56 +0545

    anchor layout

---
+++ Layouts/AnchorLayout/main.py
@@ -0,0 +1,12 @@
+
+from kivy.app import App
+from kivy.uix.widget import Widget
+from kivy.uix.anchorlayout import AnchorLayout
+
+class AnchorLayoutWidget(AnchorLayout):
+    pass
+
+class LabApp(App):
+    pass
+
+LabApp().run()
