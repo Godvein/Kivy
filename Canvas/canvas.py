4e33272a296682dbae7d8e0c86506ee5d8d67c07
Ankit Dware <sabindware@gmail.com>
2024-11-16 12:33:39 +0545

    ball animations

--- Canvas/canvas.py
+++ Canvas/canvas.py
@@ -2,6 +2,7 @@ from kivy.app import App
 from kivy.uix.widget import Widget
 from kivy.graphics import *
 from kivy.metrics import dp
+from kivy.properties import Clock
 
 class CanvasWidget(Widget):
     pass
@@ -30,8 +31,44 @@ class CanvasWidget2(Widget):
         x += inc
 
         self.rect.pos=(x, y)
+
+class CanvasWidget3(Widget):
+    def __init__(self, **kwargs):
+        self.ball_size = dp(50)
+        self.velocity_x = dp(4)
+        self.velocity_y = dp(5)
+        super().__init__(**kwargs)
+        with self.canvas:
+            self.ball = Ellipse(pos = self.center, size = (self.ball_size,self.ball_size))
+            Clock.schedule_interval(self.update, 1/60)
+
+    #called whenever the screen is resized    
+    def on_size(self, *args):
+        self.ball.pos = (self.center_x - self.ball_size/2, self.center_y - self.ball_size/2)
+
+    #called every frame
+    def update(self, dt):
+        x, y = self.ball.pos
+
+        x += self.velocity_x
+        y +=self.velocity_y
+
+        if x + self.ball_size > self.width:
+            x = self.width - self.ball_size
+            self.velocity_x = -self.velocity_x
+
+        if y + self.ball_size > self.height:
+            y = self.height - self.ball_size
+            self.velocity_y = -self.velocity_y
+        self.ball.pos = (x,y)
         
-        
+        if x < 0:
+            x = 0
+            self.velocity_x = -self.velocity_x
+
+        if y < 0:
+            y = 0
+            self.velocity_y = -self.velocity_y
 class CanvasApp(App):
     pass
 
